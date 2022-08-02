b1ef4f8ec1260d97a0b98065623c5bf7e0927f3a
Akash-1103 <84493938+Akash-1103@users.noreply.github.com>
2022-08-02 19:54:52 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello Unnati!";
+        return "Hello Unnati  this is CICD session !";
     }
 
 }
