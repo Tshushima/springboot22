6ddb25522344adc554c676372711afa20a421950
Tshushima <102301349+Tshushima@users.noreply.github.com>
2022-12-24 06:59:11 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello learners welcome to DevFest session!";
+        return "Hello learners welcome to DevFest session CI/CD!";
     }
 
 }
